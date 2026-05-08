b577ce286a2ea56b4f54f8343fc1e6757b192029
nugget <lpze.keylamarilei.gallardo.du@unifranz.edu.bo>
2026-05-08 15:13:01 -0400

    Update EstudiantesApplication.java

--- src/main/java/com/evaluacion/estudiantes/EstudiantesApplication.java
+++ src/main/java/com/evaluacion/estudiantes/EstudiantesApplication.java
@@ -1,11 +1,8 @@
 package com.evaluacion.estudiantes;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class EstudiantesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EstudiantesApplication.class, args);
 	}
