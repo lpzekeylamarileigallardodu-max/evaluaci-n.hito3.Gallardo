98d018ca0f7b47d53b24d6ec5730109ca1d5d4ab
nugget <lpze.keylamarilei.gallardo.du@unifranz.edu.bo>
2026-05-08 15:11:59 -0400

    Update Nodo.java

--- src/main/java/com/evaluacion/estudiantes/estructuras/Nodo.java
+++ src/main/java/com/evaluacion/estudiantes/estructuras/Nodo.java
@@ -1,11 +1,8 @@
 package com.evaluacion.estudiantes.estructuras;
-
 import com.evaluacion.estudiantes.model.Estudiante;
-
 public class Nodo {
     public Estudiante estudiante;
     public Nodo siguiente;
-
     public Nodo(Estudiante estudiante) {
         this.estudiante = estudiante;
         this.siguiente = null;
