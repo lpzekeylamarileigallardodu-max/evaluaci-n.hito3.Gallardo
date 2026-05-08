c0ec2abdbc599e9110a6d00d2e7f0f5ce25bb1bb
nugget <lpze.keylamarilei.gallardo.du@unifranz.edu.bo>
2026-05-08 15:12:10 -0400

    Update Estudiante.java

--- src/main/java/com/evaluacion/estudiantes/modelo/Estudiante.java
+++ src/main/java/com/evaluacion/estudiantes/modelo/Estudiante.java
@@ -1,10 +1,8 @@
 package com.evaluacion.estudiantes.model;
-
 public class Estudiante {
     public int id;
     public String nombre;
     public int puntaje;
-
     public Estudiante(int id, String nombre, int puntaje) {
         this.id = id;
         this.nombre = nombre;
