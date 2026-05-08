3742d205f7ca65825bae845ead2b5e4d73154cea
nugget <lpze.keylamarilei.gallardo.du@unifranz.edu.bo>
2026-05-08 15:12:27 -0400

    Update AulaService.java

--- src/main/java/com/evaluacion/estudiantes/servicios/AulaService.java
+++ src/main/java/com/evaluacion/estudiantes/servicios/AulaService.java
@@ -1,26 +1,19 @@
 package com.evaluacion.estudiantes.service;
-
 import org.springframework.stereotype.Service;
-
 @Service
 public class AulaService {
-
     private int[][] aula = new int[5][15];
-
     public String ocupar(int fila, int columna) {
         aula[fila][columna] = 1;
         return "Asiento ocupado: fila " + fila + " columna " + columna;
     }
-
     public String liberar(int fila, int columna) {
         aula[fila][columna] = 0;
         return "Asiento liberado: fila " + fila + " columna " + columna;
     }
-
     public int[][] mostrar() {
         return aula;
     }
-
     public int contarOcupados() {
         int count = 0;
         for (int i = 0; i < 5; i++)
